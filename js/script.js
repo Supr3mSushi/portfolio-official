584ce7b3c15fa0f9beb8fe91e455a964eded6102
Supr3mSushi <lferrand@guardiaschool.fr>
2023-04-14 01:48:14 +0200

    update

--- js/script.js
+++ js/script.js
@@ -30,8 +30,6 @@ window.onscroll = () => {
     });
 
 
-
-
 /*============= Sticky navbar =============*/
 let header = document.querySelector("header");
 
@@ -41,8 +39,29 @@ header.classList.toggle('sticky', window.scrollY > 100);
 menuIcon.classList.remove('bx-x');
 navbar.classList.remove('active');
 
-
-
 };
 
 
+/*============= Dark light mode =============*/
+
+let darkModeIcon = document.querySelector('#darkMoon-icon');
+
+darkModeIcon.onclick = () => {
+    darkModeIcon.classList.toggle('bx-sun');
+    document.body.classList.toggle('dark-mode');
+};
+
+
+/*============= Scroll reveal =============*/
+
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top'});
+ScrollReveal().reveal('.home-img img, .services-container, .projects-box, .contact form', { origin: 'bottom'});
+ScrollReveal().reveal('.home-content h1, .about-img img', { origin: 'left'});
+ScrollReveal().reveal('.home-content h3, .home-content p, .about-content', { origin: 'right'});
