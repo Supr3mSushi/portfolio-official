cfb7057feed0da892ea4d04d193d892ab8b47203
Supr3mSushi <lferrand@guardiaschool.fr>
2023-04-25 20:45:44 +0200

    update

--- js/script.js
+++ js/script.js
@@ -26,14 +26,29 @@ window.onscroll = () => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
-        };
+        }
     });
 
 
 /*============= Sticky navbar =============*/
-let header = document.querySelector("header");
 
+// let header = document.querySelector("header");
+//
+// if (window.matchMedia("(max-width: 1919px)").matches) {
+//     header.classList.add('sticky');
+// } else {
+//     header.classList.remove('sticky');
+//     header.classList.toggle('sticky', window.scrollY > 100);
+// }
+
+
+
+
+
+let header = document.querySelector("header");
 header.classList.toggle('sticky', window.scrollY > 100);
+
+
 
 /*============= Remove menu icon navbar when click navbar link (scroll) =============*/
 menuIcon.classList.remove('bx-x');
@@ -58,7 +73,7 @@ ScrollReveal({
     reset: true,
     distance: '80px',
     duration: 2000,
-    delay: 200
+    delay: 200,
 });
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top'});
@@ -68,21 +83,6 @@ ScrollReveal().reveal('.home-content h3, .home-content p, .about-content', { ori
 
 
 /*============= Modal Project =============*/
-
-/*const projectBoxes = document.querySelectorAll('.projects-box-class');
-const modal = document.querySelector('.projects-modal');
-const closeButton = modal.querySelector('.projects-modal-close');
-
-projectBoxes.forEach((box) => {
-    const seeMoreButton = box.querySelector('a');
-    seeMoreButton.addEventListener('click', () => {
-        modal.classList.add('active-modal');
-    });
-});
-
-closeButton.addEventListener('click', () => {
-    modal.classList.remove('active-modal');
-});*/
 
 const projectBoxes = document.querySelectorAll('.projects-box-class');
 const modalList = document.querySelectorAll('.projects-modal');
